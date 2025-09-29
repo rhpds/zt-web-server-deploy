07d587090e36b7081d18af2449810a93048166bd
Will Kibbe <99559012+wkibbe@users.noreply.github.com>
2025-09-29 15:09:21 -0400

    added initial website code

--- runtime-automation/03-run/setup-rhel.sh
+++ runtime-automation/03-run/setup-rhel.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
 echo "Starting module called 03-run" >> /tmp/progress.log
+
+# add website code
+echo "<h1>My Super Awesome Website</h1>" > /var/www/html/index.html
+echo "by: YOUR NAME HERE" >> /var/www/html/index.html
+echo "<p>" >> /var/www/html/index.html
+echo "My favorite meme is:" >> /var/www/html/index.html
+echo "<p>" >> /var/www/html/index.html
+echo "<img src='URL-of-your-favorite-meme'>" >>/var/www/html/index.html
