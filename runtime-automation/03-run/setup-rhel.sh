58d923ced1372448db095a7731007269e62eb6b6
Will Kibbe <99559012+wkibbe@users.noreply.github.com>
2025-10-08 18:11:18 -0400

    try new text editor

--- runtime-automation/03-run/setup-rhel.sh
+++ runtime-automation/03-run/setup-rhel.sh
@@ -8,19 +8,3 @@ echo "<p>" >> /var/www/html/index.html
 echo "My favorite meme is:" >> /var/www/html/index.html
 echo "<p>" >> /var/www/html/index.html
 echo "<img src='URL-of-your-favorite-meme'>" >>/var/www/html/index.html
-
-# make sure cockpit is installed
-dnf -y install cockpit
-# Enable cockpit functionality in showroom.
-echo "[WebService]" > /etc/cockpit/cockpit.conf
-echo "Origins = https://cockpit-${GUID}.${DOMAIN}" >> /etc/cockpit/cockpit.conf
-echo "AllowUnencrypted = true" >> /etc/cockpit/cockpit.conf
-systemctl enable --now cockpit.socket
-
-# create a user for the cockpit text editor with very limited permissions
-sudo useradd -p redhat textedit-user
-touch /var/www/html/index.html
-# Give read & write permission for index.html
-sudo setfacl -m u:textedit-user:rw /var/www/html/index.html
-
-systemctl restart cockpit
--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -3,3 +3,6 @@
 dnf -y remove httpd httpd-core httpd-filesystem
 git clone https://github.com/ellisonleao/clumsy-bird /var/www/html/clumsy-bird
 echo "DONE" >> /root/post-run.log
+
+# download sublime text for text editor
+run --rm -it --shm-size=512m -p 6901:6901 -e VNC_PW=password kasmweb/sublime-text:1.17.0
