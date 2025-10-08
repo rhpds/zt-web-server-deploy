e6d3acbccd5eb041f03272c734f58a2aa98e2761
Will Kibbe <99559012+wkibbe@users.noreply.github.com>
2025-10-08 12:06:08 -0400

    changed textedit-user configuration

--- runtime-automation/03-run/setup-rhel.sh
+++ runtime-automation/03-run/setup-rhel.sh
@@ -18,25 +18,9 @@ echo "AllowUnencrypted = true" >> /etc/cockpit/cockpit.conf
 systemctl enable --now cockpit.socket
 
 # create a user for the cockpit text editor with very limited permissions
-sudo useradd -m -s /usr/sbin/nologin textedit-user
+sudo useradd -p redhat textedit-user
 touch /var/www/html/index.html
 # Give read & write permission for index.html
 sudo setfacl -m u:textedit-user:rw /var/www/html/index.html
-
-# allow textedit-user to use cockpit without login
-tee /etc/pam.d/cockpit-nopass << EOF
-# PAM configuration for Cockpit – password‑less for textedit-user
-auth    sufficient   pam_succeed_if.so user = textedit-user
-auth    required     pam_unix.so nullok
-
-account required     pam_unix.so
-password required    pam_unix.so
-session required     pam_unix.so
-EOF
-
-tee /etc/cockpit/cockpit.conf  << EOF
-[WebService]
-pam-service = cockpit-nopass
-EOF
 
 systemctl restart cockpit
