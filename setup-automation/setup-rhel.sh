96a056b14474cecbbe38de0cff03f0fccaaa350c
Will Kibbe <99559012+wkibbe@users.noreply.github.com>
2025-10-09 08:19:24 -0400

    switched to terminal-based text editor

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -3,3 +3,5 @@
 dnf -y remove httpd httpd-core httpd-filesystem
 git clone https://github.com/ellisonleao/clumsy-bird /var/www/html/clumsy-bird
 echo "DONE" >> /root/post-run.log
+
+dnf install -y nano vim
