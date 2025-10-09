ba93b55c3e71dc43e796c23519dbdf303d10560e
Will Kibbe <99559012+wkibbe@users.noreply.github.com>
2025-10-09 08:16:42 -0400

    removes RHEL web console/GUI text editor

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -3,6 +3,3 @@
 dnf -y remove httpd httpd-core httpd-filesystem
 git clone https://github.com/ellisonleao/clumsy-bird /var/www/html/clumsy-bird
 echo "DONE" >> /root/post-run.log
-
-# download sublime text for text editor
-podman run --rm -it --shm-size=512m -p 6901:6901 -e VNC_PW=password kasmweb/sublime-text:1.17.0
