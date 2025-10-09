0ca1ae0966b1a544563761cfff57e3a71982469a
Will Kibbe <99559012+wkibbe@users.noreply.github.com>
2025-10-09 07:53:19 -0400

    Revert "removed text editor from setup script"
    This reverts commit 2ad059b4cbb818f05e3bf210e1da81599d06e40e.

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -5,4 +5,4 @@ git clone https://github.com/ellisonleao/clumsy-bird /var/www/html/clumsy-bird
 echo "DONE" >> /root/post-run.log
 
 # download sublime text for text editor
-# podman run --rm -it --shm-size=512m -p 6901:6901 -e VNC_PW=password kasmweb/sublime-text:1.17.0
+podman run --rm -it --shm-size=512m -p 6901:6901 -e VNC_PW=password kasmweb/sublime-text:1.17.0
